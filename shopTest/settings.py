8c931317de2f236e0d3089940601da7817fa8aaa
2023520968 <849719533@qq.com>
2023-12-22 23:00:58 +0800

    部署

--- shopTest/settings.py
+++ shopTest/settings.py
@@ -150,13 +150,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR,'static') # 收集的到feng_static目录
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
