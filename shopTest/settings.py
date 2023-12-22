963a9fff7b09a90c5802912c65231cc45e234a7c
2023520968 <849719533@qq.com>
2023-12-22 23:04:55 +0800

    部署

--- shopTest/settings.py
+++ shopTest/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
