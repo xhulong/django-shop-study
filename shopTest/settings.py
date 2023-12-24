f818034e8e3b5520e89dc25e738161ff8e6a97f7
2023520968 <849719533@qq.com>
2023-12-24 17:27:16 +0800

    部署

--- shopTest/settings.py
+++ shopTest/settings.py
@@ -100,13 +100,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': "tabzxiaohu",
+#         'USER': 'tabzxiaohu',
+#         'PASSWORD': 'tabzxiaohu',
+#         'HOST': 'db',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "tabzxiaohu",
-        'USER': 'tabzxiaohu',
-        'PASSWORD': 'tabzxiaohu',
-        'HOST': 'db',
+        'NAME': "web_shop",
+        'USER': 'root',
+        'PASSWORD': '123123',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
